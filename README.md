c7f217c9af78d9a94bf25038c6f6922e71cf2d63
VelonacepsCalyxEggs <ivankuvaev777@gmail.com>
2024-11-16 19:03:21 +0300

    Code cleanup and QoL

---
+++ README.md
@@ -0,0 +1,34 @@
+# Nuclear Option Mission Generator
+
+This is a mission generator for the game "Nuclear Option".
+
+## Description
+
+"Nuclear Option" is a game where you fly near-future aircraft with immersive physics on intense battlefields, facing land, air, and sea threats. Wage war against AI or other players with an array of potent weapons. Wield tactical and strategic nuclear weapons, capable of annihilating anything in their path.
+
+This repository utilizes the game's flexible .json structure of missions, to create quick missions from scratch.
+
+## Features
+
+- Generate missions with custom parameters.
+- Create diverse scenarios involving aircraft and naval vessels.
+- Set faction-specific aircraft and ship placements.
+- Modify mission objectives. Soon... 
+
+## Installation
+
+1. Clone the repository:
+   ```sh
+   git clone https://github.com/yourusername/nuclear-option-mission-generator.git
+   ```
+2. Navigate to project directory:
+    ```sh
+    cd NO_MissionGen
+    ```
+
+
+## Usage
+Run the missionGen.py file:
+    ```sh
+    python missionGen.py
+    ```
--- missionGen.py
+++ missionGen.py
@@ -2,6 +2,8 @@ import json
 import copy
 import random
 import math
+import os
+import shutil
 
 TEAM_SIZE = 4
 PLAYER_TEAM = ""
@@ -16,7 +18,6 @@ Z_BOSCALI = 0
 X_PRIMEVA = 0
 Y_PRIMEVA = 300
 Z_PRIMEVA = 10000
-
 class Aircraft:
     def __init__(self, type, faction, unique_name, x, y, z, rotation_y, livery, weapon_selections, skill_range, bravery_range, speed_range):
         self.type = type
@@ -55,134 +56,100 @@ class Aircraft:
             "startingSpeed": self.starting_speed
         }
 
-# Define specific templates for each aircraft type
-# Also, it is a pain getting all the avaivable weapon selections... there should be an easier way...
-aircraft_templates = {
-    "COIN": {"weapon_selections": [1, 2, 6, 4], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (100.0, 150.0)},
-    "trainer": {"weapon_selections": [1, 2, 3, 4], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (250.0, 400.0)},
-    "Multirole1": {"weapon_selections": [1, 3, 5, 7], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (400.0, 600.0)},
-    "Fighter1": {"weapon_selections": [2, 4, 6, 8], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (400.0, 600.0)},
-    "AttackHelo1": {"weapon_selections": [0, 1, 2, 3], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (100.0, 200.0)},
-    "Darkreach": {"weapon_selections": [3, 4, 5, 6], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (200.0, 320.0)},
-    "EW1": {"weapon_selections": [1, 2, 3, 4], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (250.0, 350.0)},
-    "QuadVTOL1": {"weapon_selections": [0, 2, 4, 6], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (100.0, 150.0)}
-}
 
-# Load the mission template
-with open("./templates/mission_template.json", "r") as f:
-    mission_template = json.loads(f.read())
+def copy_mission_file_to_game(mission):
+    try:
+        local_low_path = os.path.expandvars("%USERPROFILE%/AppData/LocalLow/Shockfront/NuclearOption/Missions/QuickGenMission")
+        
+        if not os.path.exists(local_low_path):
+            return print("Please make a mission titled 'QuickGenMission'!")
+        with open(local_low_path + "/QuickGenMission.json", "w") as f:
+            json.dump(mission, f, indent=4)
+        print("Mission files ported to game successfully.")
+    except IOError as e:
+        print(e)
 
-with open("./templates/ship_template.json", "r") as f:
-    ship_templates = json.loads(f.read())
+def load_json(json_path):
+    with open(json_path, "r") as f:
+        return json.loads(f.read())
 
+def assign_airbases(mission):
+    for airbase in (mission["airbases"]):
+        airbase["faction"] = random.choice(FACTIONS)
 
-# Create the mission
-mission = copy.deepcopy(mission_template)
+def add_fleets(ship_templates, mission):
+    # Add fleets
+    def calculate_endpoint(length, angle):
+        # Convert angle to radians if it's in degrees
+        # Wibbley wobbley timey wimey trigonometry stuff...
+        angle_radians = math.radians(angle)
+        x = length * math.cos(angle_radians)
+        z = length * math.sin(angle_radians)
+        return x, z 
 
-with open("./templates/objective_templates.json", "r") as f:
-    objective_templates = json.load(f)
+    # Add fleets with 4 corvettes and 1 destroyer around each carrier
+    for i in range(1, 3):  # Assuming 2 carriers, constant it later
+        if i % 2 == 0:
+            carrier_faction = FACTIONS[0]
+        else:
+            carrier_faction = FACTIONS[1]
+        
+        carrier = copy.deepcopy(ship_templates[0])
+        random_angle = random.randint(0, 359)
+        carrier_x, carrier_z = calculate_endpoint(60000, random_angle)
+        carrier["globalPosition"]["x"] = carrier_x
+        carrier["globalPosition"]["z"] = carrier_z
+        carrier["faction"] = carrier_faction
+        carrier["UniqueName"] = "FleetCarrier" + str(i)
+        print(carrier["UniqueName"])
+        mission["ships"].append(carrier)
 
+        # Place 4 corvettes around the carrier
+        for j in range(4):
+            angle = random_angle + (j * 90)
+            corvette_x, corvette_z = calculate_endpoint(5000, angle)  # 5000 units away from carrier
+            corvette = copy.deepcopy(ship_templates[1])
+            corvette["globalPosition"]["x"] = carrier_x + corvette_x
+            corvette["globalPosition"]["z"] = carrier_z + corvette_z
+            corvette["faction"] = carrier_faction
+            corvette["UniqueName"] = f"Corvette{i}_{j+1}"
+            mission["ships"].append(corvette)
+            print(corvette["UniqueName"], "at", corvette["globalPosition"])
 
+        # Place 1 destroyer 10000 units away from the carrier
+        destroyer_angle = random_angle + 45  # Place it diagonally
+        destroyer_x, destroyer_z = calculate_endpoint(10000, destroyer_angle)
+        destroyer = copy.deepcopy(ship_templates[2])
+        destroyer["globalPosition"]["x"] = carrier_x + destroyer_x
+        destroyer["globalPosition"]["z"] = carrier_z + destroyer_z
+        destroyer["faction"] = carrier_faction
+        destroyer["UniqueName"] = f"Destroyer{i}"
+        mission["ships"].append(destroyer)
+        print(destroyer["UniqueName"], "at", destroyer["globalPosition"])
 
-# Assign airbases.
-for airbase in (mission["airbases"]):
-    airbase["faction"] = random.choice(FACTIONS)
-    #print(f"{airbase["DisplayName"]} was set to faction {airbase["faction"]}.")
+def place_aircraft(x_pos, y_pos, z_pos, rot_y, team_faction, aircraft_templates):
+    # Add aircraft
+    aircraft_list = []
+    for i in range(1, TEAM_SIZE + 1):
+        type_choice = random.choice(list(aircraft_templates.keys()))
+        template = aircraft_templates[type_choice]
+        aircraft = Aircraft(type=type_choice, 
+                            faction=team_faction, 
+                            unique_name=f"{team_faction}_" + str(i), 
+                            x = x_pos, 
+                            y = y_pos,
+                            z= z_pos, 
+                            rotation_y=rot_y, 
+                            livery=3, 
+                            weapon_selections=template["weapon_selections"],
+                            skill_range=template["skill_range"], 
+                            bravery_range=template["bravery_range"], 
+                            speed_range=template["speed_range"])
+        aircraft_list.append(aircraft.to_dict())
+        x_pos += 25
+    return aircraft_list
 
-# Add fleets
-def calculate_endpoint(length, angle):
-    # Convert angle to radians if it's in degrees
-    # Wibbley wobbley timey wimey trigonometry stuff...
-    angle_radians = math.radians(angle)
-    x = length * math.cos(angle_radians)
-    z = length * math.sin(angle_radians)
-    return x, z 
-
-# Add fleets with 4 corvettes and 1 destroyer around each carrier
-for i in range(1, 3):  # Assuming 2 carriers, constant it later
-    if i % 2 == 0:
-        carrier_faction = FACTIONS[0]
-    else:
-        carrier_faction = FACTIONS[1]
-    
-    carrier = copy.deepcopy(ship_templates[0])
-    random_angle = random.randint(0, 359)
-    carrier_x, carrier_z = calculate_endpoint(60000, random_angle)
-    carrier["globalPosition"]["x"] = carrier_x
-    carrier["globalPosition"]["z"] = carrier_z
-    carrier["faction"] = carrier_faction
-    carrier["UniqueName"] = "FleetCarrier" + str(i)
-    print(carrier["UniqueName"])
-    mission["ships"].append(carrier)
-
-    # Place 4 corvettes around the carrier
-    for j in range(4):
-        angle = random_angle + (j * 90)
-        corvette_x, corvette_z = calculate_endpoint(5000, angle)  # 5000 units away from carrier
-        corvette = copy.deepcopy(ship_templates[1])
-        corvette["globalPosition"]["x"] = carrier_x + corvette_x
-        corvette["globalPosition"]["z"] = carrier_z + corvette_z
-        corvette["faction"] = carrier_faction
-        corvette["UniqueName"] = f"Corvette{i}_{j+1}"
-        mission["ships"].append(corvette)
-        print(corvette["UniqueName"], "at", corvette["globalPosition"])
-
-    # Place 1 destroyer 10000 units away from the carrier
-    destroyer_angle = random_angle + 45  # Place it diagonally
-    destroyer_x, destroyer_z = calculate_endpoint(10000, destroyer_angle)
-    destroyer = copy.deepcopy(ship_templates[2])
-    destroyer["globalPosition"]["x"] = carrier_x + destroyer_x
-    destroyer["globalPosition"]["z"] = carrier_z + destroyer_z
-    destroyer["faction"] = carrier_faction
-    destroyer["UniqueName"] = f"Destroyer{i}"
-    mission["ships"].append(destroyer)
-    print(destroyer["UniqueName"], "at", destroyer["globalPosition"])
-
-
-# Add Boscali aircraft
-for i in range(1, TEAM_SIZE + 1):
-    type_choice = random.choice(list(aircraft_templates.keys()))
-    template = aircraft_templates[type_choice]
-    aircraft = Aircraft(type=type_choice, 
-                        faction="Boscali", 
-                        unique_name="Boscali_" + str(i), 
-                        x=X_BOSCALI, 
-                        y = Y_BOSCALI, 
-                        z=Z_BOSCALI, 
-                        rotation_y=0, 
-                        livery=0, 
-                        weapon_selections=template["weapon_selections"],
-                        skill_range=template["skill_range"], 
-                        bravery_range=template["bravery_range"], 
-                        speed_range=template["speed_range"])
-    mission["aircraft"].append(aircraft.to_dict())
-    X_BOSCALI += 25
-    print(i)
-
-# Add Primeva aircraft
-for i in range(1, TEAM_SIZE + 1):
-    type_choice = random.choice(list(aircraft_templates.keys()))
-    template = aircraft_templates[type_choice]
-    aircraft = Aircraft(type=type_choice, 
-                        faction="Primeva", 
-                        unique_name="Primeva_" + str(i), 
-                        x=X_PRIMEVA, 
-                        y = Y_PRIMEVA,
-                        z=Z_PRIMEVA, 
-                        rotation_y=1, 
-                        livery=3, 
-                        weapon_selections=template["weapon_selections"],
-                        skill_range=template["skill_range"], 
-                        bravery_range=template["bravery_range"], 
-                        speed_range=template["speed_range"])
-    mission["aircraft"].append(aircraft.to_dict())
-    X_PRIMEVA += 25
-    print(i)
-
-random.choice(mission["aircraft"])["playerControlled"] = True
-
-
-def editObjectives():
+def edit_objectives(mission, objective_templates):
     allBoscaliAircraft = []
     allPrimevaAircraft = []
 
@@ -220,9 +187,55 @@ def editObjectives():
             objective["Data"].extend(allPrimevaAircraft)  
 
     mission["objectives"] = objective_templates[0]
-editObjectives()
 
-# Write the mission to the file
-with open('C:/Users/ivank/Documents/NO_MIssionGen/mission.json', 'w') as f:
-    json.dump(mission, f, indent=4)
-    print("Mission generated succsessfully.")
+def main():
+    mission_template = load_json("./templates/mission_template.json")
+    ship_templates = load_json("./templates/ship_template.json")
+    objective_templates = load_json("./templates/objective_templates.json")
+    
+    # It is a pain getting all the avaivable weapon selections... there should be an easier way...
+    aircraft_templates = {
+        "COIN": {"weapon_selections": [1, 2, 6, 4], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (100.0, 150.0)},
+        "trainer": {"weapon_selections": [1, 2, 3, 4], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (250.0, 400.0)},
+        "Multirole1": {"weapon_selections": [1, 3, 5, 7], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (400.0, 600.0)},
+        "Fighter1": {"weapon_selections": [2, 4, 6, 8], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (400.0, 600.0)},
+        "AttackHelo1": {"weapon_selections": [0, 1, 2, 3], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (100.0, 200.0)},
+        "Darkreach": {"weapon_selections": [3, 4, 5, 6], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (200.0, 320.0)},
+        "EW1": {"weapon_selections": [1, 2, 3, 4], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (250.0, 350.0)},
+        "QuadVTOL1": {"weapon_selections": [0, 2, 4, 6], "skill_range": (1.0, 5.0), "bravery_range": (0.0, 1.0), "speed_range": (100.0, 150.0)}
+    }
+
+
+    if not mission_template or not ship_templates or not objective_templates:
+        return
+
+    mission = copy.deepcopy(mission_template)
+
+    assign_airbases(mission)
+
+    add_fleets(ship_templates, mission)
+    rotation_y_boscali = 0.0
+    rotation_y_primeva = 1.0
+    boscali_aircraft = place_aircraft(X_BOSCALI, Y_BOSCALI, Z_BOSCALI, rotation_y_boscali, FACTIONS[1], aircraft_templates)
+    primeva_aircraft = place_aircraft(X_PRIMEVA, Y_PRIMEVA, Z_PRIMEVA, rotation_y_primeva, FACTIONS[0], aircraft_templates)
+    print(boscali_aircraft)
+    mission["aircraft"].extend(boscali_aircraft)
+    mission["aircraft"].extend(primeva_aircraft)
+    
+    # Choose a random aircraft for a player to control. 
+    # I have priorities listed out for later use.
+    random.choice(mission["aircraft"])["playerControlled"] = True
+
+    edit_objectives(mission, objective_templates)
+
+    try:
+        with open('C:/Users/ivank/Documents/NO_MIssionGen/mission.json', 'w') as f:
+            json.dump(mission, f, indent=4)
+            print("Mission generated successfully.")
+            copy_mission_file_to_game(mission)
+    except IOError as e:
+        print("Error: Failed to write mission to file.")
+        print(e)
+
+if __name__ == "__main__":
+    main()
